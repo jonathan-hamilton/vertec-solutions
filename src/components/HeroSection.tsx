fba896a4ef12f7e33ce57d7d80561666ed7b20bb
Jonathan Hamilton <jhamilton.bsit@gmail.com>
2025-06-28 18:21:02 -0700

    Sticky Brand, Hero Scale away

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,89 +5,66 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const HeroSection = () => {
   const { scrollY } = useScroll();
   const opacity = useTransform(scrollY, [0, 600, 1000], [1, 1, 0]);
-  const scale = useTransform(scrollY, [0, 1000], [1, 0.8]);
+  const scale = useTransform(scrollY, [0, 1000], [1, 0.4]);
 
   return (
-    <Box sx={{ position: "sticky", top: 0, zIndex: -1 }}>
-      <motion.div style={{ opacity }}>
-        <Container
+    <motion.div style={{ opacity, scale, transformOrigin: "bottom center" }}>
+      <Container maxWidth="xl" sx={{ px: 0 }}>
+        <Box
           sx={{
-            textAlign: "center",
-            pt: 6,
-            pb: 2,
-            backgroundColor: "background.default",
+            minHeight: "70vh",
+            height: "60vh",
+            backgroundImage: `url(${bridgeImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            position: "relative",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 4,
+            mb: 20,
+            overflow: "hidden",
+            "&::before": {
+              content: '""',
+              position: "absolute",
+              inset: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+              zIndex: 0,
+            },
           }}
         >
-          <Typography
-            variant="h1"
-            sx={{
-              fontWeight: 900,
-              fontSize: { xs: "3rem", md: "6rem" },
-              color: "black",
-            }}
-          >
-            Vertec Solutions
-          </Typography>
-        </Container>
-      </motion.div>
-
-      <motion.div style={{ opacity, scale }}>
-        <Container maxWidth="xl" sx={{ px: 0 }}>
           <Box
             sx={{
-              minHeight: "70vh",
-              height: "60vh",
-              backgroundImage: `url(${bridgeImage})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
               position: "relative",
-              color: "white",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: 4,
-              mb: 20,
-              overflow: "hidden",
-              "&::before": {
-                content: '""',
-                position: "absolute",
-                inset: 0,
-                backgroundColor: "rgba(0, 0, 0, 0.4)",
-                zIndex: 0,
-              },
+              zIndex: 1,
+              textAlign: "center",
+              px: 2,
             }}
           >
-            <Box
-              sx={{
-                position: "relative",
-                zIndex: 1,
-                textAlign: "center",
-                px: 2,
-              }}
+            <Typography variant="h2" fontWeight={700} gutterBottom>
+              Better design, better experiences.
+            </Typography>
+            <Typography variant="h6" paragraph>
+              As website and web application developers, we understand the
+              perfect user interface should look good and work even better.
+              Alongside our clients, we uncover problems and solve them. In
+              short, we create better online experiences.
+            </Typography>
+            <Button
+              href="/contact"
+              variant="contained"
+              color="primary"
+              size="large"
+              sx={{ mt: 3 }}
             >
-              <Typography variant="h2" fontWeight={700} gutterBottom>
-                Better design, better experiences.
-              </Typography>
-              <Typography variant="h6" paragraph>
-                As website and web application developers, we understand the
-                perfect user interface should look good and work even better.
-                Alongside our clients, we uncover problems and solve them. In
-                short, we create better online experiences.
-              </Typography>
-              <Button
-                href="/contact"
-                variant="contained"
-                color="primary"
-                size="large"
-                sx={{ mt: 3 }}
-              >
-                Contact
-              </Button>
-            </Box>
+              Contact
+            </Button>
           </Box>
-        </Container>
-      </motion.div>
-    </Box>
+        </Box>
+      </Container>
+    </motion.div>
+    // </Box>
   );
 };
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,9 +5,14 @@ import WorkSection from "../components/WorkSection";
 import CTASection from "../components/CTASection";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { Box, Container, Typography, useTheme } from "@mui/material";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Profile() {
   const location = useLocation();
+  const { scrollY } = useScroll();
+  const opacity = useTransform(scrollY, [0, 200, 400], [1, 1, 0]);
+  const theme = useTheme();
 
   useEffect(() => {
     if (location.hash) {
@@ -23,6 +28,29 @@ export default function Profile() {
 
   return (
     <>
+      <Box sx={{ position: "sticky", top: 0, zIndex: -1 }}>
+        <motion.div style={{ opacity }}>
+          <Container
+            sx={{
+              textAlign: "center",
+              pt: 6,
+              pb: 2,
+              backgroundColor: "background.default",
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                fontWeight: 900,
+                fontSize: { xs: "3rem", md: "6rem" },
+                color: theme.palette.text.secondary,
+              }}
+            >
+              Vertec Solutions
+            </Typography>
+          </Container>
+        </motion.div>
+      </Box>
       <div id="hero">
         <HeroSection />
       </div>
